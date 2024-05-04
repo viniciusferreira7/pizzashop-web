d854a24b356a19496a3f2296384e7b5308beeb0f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-05-04 11:24:39 -0300

    feat: create page layouts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './_layouts/app'
+import { AuthLayout } from './_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
   },
   {
-    path: '/sign-in',
-    element: <SignIn />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
   },
 ])
