282c0480edf79802316d0055332b05d9b87d7574
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-05-08 13:00:23 -0300

    fix: creates a login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './_layouts/app'
-import { AuthLayout } from './_layouts/auth'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
