597a9d3a304895d85ce0f24fc04514c39390485f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-13 10:36:09 -0300

    feat: creates an optimistic interface

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -25,7 +25,7 @@ import { Textarea } from './ui/textarea'
 
 const storeProfileSchema = z.object({
   name: z.string().min(1),
-  description: z.string(),
+  description: z.string().nullable(),
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
@@ -51,22 +51,38 @@ export function StoreProfileDialog() {
     },
   })
 
+  function updateManagedRestaurantCached({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      'managed-restaurant',
+    ])
+
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ['managed-restaurant'],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      )
+    }
+
+    return { cached }
+  }
+
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    onSuccess: (_, { name, description }) => {
-      const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
-        'managed-restaurant',
-      ])
+    onMutate: ({ name, description }) => {
+      const { cached } = updateManagedRestaurantCached({ name, description })
 
-      if (cached) {
-        queryClient.setQueryData<GetManagedRestaurantResponse>(
-          ['managed-restaurant'],
-          {
-            ...cached,
-            name,
-            description,
-          },
-        )
+      return { previousProfile: cached }
+    },
+    onError: (_error, _variables, context) => {
+      if (context?.previousProfile) {
+        updateManagedRestaurantCached(context.previousProfile)
       }
     },
   })
