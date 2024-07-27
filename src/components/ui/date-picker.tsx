03675ce76266c2bd1e89b8164133a93e2ea703f1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-27 08:24:31 -0300

    feat: adds the revenue in the period

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import dayjs from 'dayjs'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import * as React from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>()
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-[280px] justify-start text-left font-normal',
+            !date && 'text-muted-foreground',
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? (
+            dayjs(date).format('dddd MMMM DD[th , ]YYYY ')
+          ) : (
+            <span>Pick a date</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
---
+++ src/components/ui/date-range-picker.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import dayjs from 'dayjs'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import { ComponentProps } from 'react'
+import { DateRange } from 'react-day-picker'
+
+import { Button } from '@/components/ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+import { Calendar } from './calendar'
+
+interface DateRangePickerProps extends ComponentProps<'div'> {
+  date: DateRange | undefined
+  onDateChange: (date: DateRange | undefined) => void
+}
+
+export function DateRangePicker({
+  className,
+  date,
+  onDateChange,
+}: DateRangePickerProps) {
+  return (
+    <div className={cn('grid gap-2', className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={'outline'}
+            className={cn(
+              'w-[300px] justify-start text-left font-normal',
+              !date && 'text-muted-foreground',
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {dayjs(date.from).format('MMM DD[, ] YYYY')} -{' '}
+                  {dayjs(date.to).format('MMM DD[, ] YYYY')}
+                </>
+              ) : (
+                dayjs(date.from).format('MMM DD[, ] YYYY')
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={onDateChange}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
